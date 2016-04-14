ecb204f6073b031c6ebb0398431f637c0802aba4
Martyn Coupland <martyn.coupland@inframon.com>
2016-04-14 09:44:45 +0100

    Initial commit

---
+++ Deploy-ResourceGroupTemplate.ps1
@@ -0,0 +1,8 @@
+﻿# Login to Azure
+Login-AzureRmAccount
+
+# Get the resource group
+$ResourceGroup = Get-AzureRmResourceGroup -ResourceGroupName "TheCloudGuy"
+
+# Deploy the template to the resource group
+New-AzureRmResourceGroupDeployment -ResourceGroupName $ResourceGroup -TemplateFile "<Path To Template>.json"
